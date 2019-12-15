63761547953e431a6785b542e42f7d2f8c57a2e6
Ben Arnold <ben@arnoldfamily.me.uk>
2019-12-15 21:53:15 +0000

    Finished 2019 day 3

--- AdventOfCode/2019/03-CrossedWires.cs
+++ AdventOfCode/2019/03-CrossedWires.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Xunit;
+using static System.Math;
 
 namespace AdventOfCode
 {
@@ -61,6 +62,7 @@ namespace AdventOfCode
         [InlineData(4, -2, "2,3,4")]
         [InlineData(-1, 3, "-1,0,1,2")]
         [InlineData(-3, -2, "-5,-4,-3")]
+        [InlineData(5, -10, "-5,-4,-3,-2,-1,0,1,2,3,4,5")]
         public void MovementRange(int initial, int move, string range)
             => string.Join(",", sut.MovementRange(initial, move)).Should().Be(range);
 
@@ -71,16 +73,20 @@ namespace AdventOfCode
             new object[] { "R5,U5,L5", "U6,R2,D6", new[] { (0,5), (2,5), (2,0) } },
         };
 
-
         [Theory]
         [MemberData(nameof(Crosses))]
         public void FindCrosses(string path1, string path2, (int, int)[] crosses)
             => sut.FindCrosses(path1, path2).Should().BeEquivalentTo(crosses);
-        //[Theory]
-        //[InlineData("R1,U1", "U1,R1", 2)]
-        //[InlineData("R2,U1", "U2,R1", 3)]
-        //public void Calculate(string path1, string path2, int closestCross)
-        //    => sut.Calculate(path1, path2).Should().Be(closestCross);
+
+        [Theory]
+        [InlineData("R1,U1", "U1,R1", 2)]
+        [InlineData("R5,U3", "U2,R7", 7)]
+        [InlineData("U7,R6,D4,L4", "R8,U5,L5,D3", 6)]
+        [InlineData("U5,R2,D10", "D2,R4", 4)]
+        [InlineData("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159)]
+        [InlineData("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)]
+        public void Calculate(string path1, string path2, int closestCross)
+            => sut.Calculate(path1, path2).Should().Be(closestCross);
     }
 
     public class CrossedWires
@@ -111,28 +117,37 @@ namespace AdventOfCode
         {
             var segments = ParsePath(path);
             return segments.Aggregate(
-                (x: 0, y: 0, steppedOn: new List<(int x, int y)>()), 
-                (acc, xy) =>
-            {
-                acc.steppedOn.AddRange(
-                    MovementRange(acc.x, xy.x, x => (x, acc.y)));
-                acc.steppedOn.AddRange(
-                    MovementRange(acc.y, xy.y, y => (acc.x, y)));
-                return (acc.x + xy.x, acc.y + xy.y, acc.steppedOn);
-            }, x => x.steppedOn.Distinct().ToArray());
+                (x: 0, y: 0, touched: new List<(int x, int y)>()),
+                AddMovements,
+                acc => acc.touched.Distinct().ToArray());
+
+            (int, int, List<(int x, int)>) AddMovements(
+                (int x, int y, List<(int x, int y)> touched) acc,
+                (int x, int y) i)
+                => (acc.x + i.x, acc.y + i.y
+                   , acc.touched
+                        .Union(AddMovement(acc.x, acc.y, i.x, i.y))
+                        .ToList());
+
+            IEnumerable<(int x, int accy)> AddMovement(
+                int curX, int curY, int moveX, int moveY)
+                => MovementRange(curX, moveX, x => (x, curY)).Union(
+                   MovementRange(curY, moveY, y => (curX, y)));
         }
 
         private IEnumerable<(int x, int accy)> MovementRange(
-            int accx, int xyx, Func<int, (int, int)> selector) 
+            int accx, int xyx, Func<int, (int, int)> selector)
             => MovementRange(accx, xyx).Select(selector);
 
-        internal int[] MovementRange(int a, int b)
+        internal int[] MovementRange(int start, int count)
         {
-            var (start, count) = b < 0
-                ? (a + b, (b * -1) + 1)
-                : (a, b + 1);
+            var isReverseMovement = count < 0;
 
-            return Enumerable.Range(start, count).ToArray();
+            var (start_, count_) = isReverseMovement
+                ? (start + count, (count * -1))
+                : (start, count);
+
+            return Enumerable.Range(start_, count_ + 1).ToArray();
         }
 
         internal (int x, int y)[] FindCrosses(string path1, string path2)
@@ -141,7 +156,13 @@ namespace AdventOfCode
             var paves2 = ParsePathToGraph(path2);
             var intersection = paves1.Intersect(paves2).ToList();
             intersection.Remove((0, 0));
-            return intersection.ToArray(); ;
+            return intersection.ToArray();
         }
+
+        internal int Calculate(string path1, string path2)
+            => FindCrosses(path1, path2)
+                .Select(cross => Abs(cross.x) + Abs(cross.y))
+                .DefaultIfEmpty(0)
+                .Min();
     }
 }
